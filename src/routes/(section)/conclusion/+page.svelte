efc699edcceba73b95907fcdb198f22c6e3a0646
Jeev Prayaga <findjeev@gmail.com>
2023-05-15 19:48:35 -0500

    add search results img to conclusion

--- src/routes/(section)/conclusion/+page.svelte
+++ src/routes/(section)/conclusion/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import copy from '../../../data/copy.json';
 	import Paragraph from '../Paragraph.svelte';
+	import { fade } from 'svelte/transition';
+	import Figure from '../Figure.svelte';
 </script>
 
 <section class="grid-wrapper page">
@@ -8,6 +10,22 @@
 	<Paragraph words={copy.conclusion1} classProp="start3 end9" />
 	<Paragraph words={copy.conclusion2} classProp="start3 end9" />
 	<Paragraph words={copy.conclusion3} classProp="start3 end9" />
+	{#if true}
+		<div class="start2 end7 search-results" in:fade={{ duration: 300, delay: 1200 }}>
+			<Figure
+				source="./search-results1.png"
+				alt="Google search results for the term 'metaverse' on May 12, 2022"
+				caption="Google search results for the term 'metaverse' on May 12, 2022"
+			/>
+		</div>
+		<div class="start7 end12 search-results" in:fade={{ duration: 300, delay: 1200 }}>
+			<Figure
+				source="./search-results2.png"
+				alt="Google search results for the term 'metaverse' on May 15, 2023"
+				caption="Google search results for the term 'metaverse' on May 15, 2023"
+			/>
+		</div>
+	{/if}
 	<Paragraph words={copy.conclusion4} classProp="start3 end9" />
 	<Paragraph words={copy.conclusion5} classProp="start3 end9" />
 </section>
@@ -19,5 +37,9 @@
 
 	h1 {
 		margin-bottom: 2em;
+	}
+
+	.search-results {
+		margin: 3em 0;
 	}
 </style>
